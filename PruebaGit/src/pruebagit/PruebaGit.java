549e765f4d60d96a823d34b03ac9bedf820f7c67
andrums5 <andrums5@hotmail.com>
2016-04-11 12:31:36 +0200

    primer cambio

---
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author usu21
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
