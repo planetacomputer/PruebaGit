e0704754a31d564903a71b73179ddc48a67a1db1
andrums5 <andrums5@hotmail.com>
2016-04-11 12:33:08 +0200

    hola mundo 2

--- PruebaGit/src/pruebagit/PruebaGit.java
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -16,6 +16,7 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola mundo 2");
         // TODO code application logic here
     }
     
